f6ad18910fbc94336ddd4f27494ddd23f44e869e
renchao <renchao@renchaodeMacBook-Pro.local>
2017-06-16 01:02:13 +0800

    初始化工程

---
+++ STAAnimation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  STAAnimation
+//
+//  Created by renchao on 2017/6/16.
+//  Copyright © 2017年 renchao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ STAAnimation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  STAAnimation
+//
+//  Created by renchao on 2017/6/16.
+//  Copyright © 2017年 renchao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
